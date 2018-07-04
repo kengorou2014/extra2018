ae71b5c6133a4197d30a97b7e7c7742693a73784
sunagimon <TK.impo.tanshio0701@gmail.com>
2018-07-04 13:09:03 +0900

    explosion

---
+++ Assets/Explosion/Explosion.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour {
+    public GameObject ExploadObj;
+    public GameObject ExploadPos;
+    // Update is called once per frame
+    void Update () {
+        //スペースキーを押したら
+        if (Input.GetButtonDown ("Jump")) {
+            Instantiate (ExploadObj, ExploadPos.transform.position, Quaternion.identity);
+
+        }
+    }
+}
